52441cb034ffe2d096113b08390460d2738a0531
MaiconFilipiaki <mdfilipiaki@gmail.com>
2019-03-10 19:29:53 -0300

    Mudanca no nome do arquivo de saida

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 arquivo = input('Digite o nome do arquivo: ')
-with open(arquivo, 'r') as arquivo_existente, open('novo_arquivo.txt',
+with open(arquivo, 'r') as arquivo_existente, open('saida.txt',
                                                    'w') as novo_arquivo:
     for linha in arquivo_existente.readlines():
         novo_arquivo.write(linha)
